f843ef821611242476d93177eaa1e260eb599edb
songtao <songtao@snailgame.net>
2016-12-28 10:56:13 +0800

    update

---
+++ room/create.js
@@ -0,0 +1,20 @@
+(function() {
+
+var vm = new Vue({
+    el : '#page',
+    data : {
+        name : 'AAA',
+        order : 2,
+        tag : '',
+        userNum : 2,
+        desc : '',
+        thumb : './image.png',
+        u3dbg : './image.png',
+        dependencyCharge : 0,
+        chargeStrategy  : '',
+        onlineRatio : 1,
+        channelId : 1
+    }
+})
+
+})()
