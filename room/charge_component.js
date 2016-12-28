49361e67e8413dbf2fa7c2ed27b793153c2c7022
songtao <songtao@snailgame.net>
2016-12-28 14:41:36 +0800

    update charge_component.js

---
+++ room/charge_component.js
@@ -0,0 +1,27 @@
+Vue.component('charge',{
+    template : '<div class="inline">\
+                    <input type="number" class="charge-input-money" v-model="m"/>\
+                        <span class="charge-span">元</span>\
+                        <input type="number" class="charge-input-duration" v-model="d"/>\
+                        <select class="charge-input" v-model="u">\
+                            <option value="day">天</option>\
+                            <option value="mouth">月</option>\
+                            <option value="year">年</option>\
+                        </select>\
+                        <div class="negative ui button delete-btn"> <i class="minus icon delete-icon"></i>删除</div>\
+                </div>',
+    props : ['money','duration','unit'],
+    data : function() {
+        return {
+            m : this.money || 30,
+            d : this.duration || 20,
+            u : this.unit || 'day'
+        }
+    },
+    methods : {
+        isSelect : function(u) {
+            console.log(u)
+            return this.u == u ? 'selected' : '';
+        }
+    }
+});
