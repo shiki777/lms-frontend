16d58994a40b87b92d91166a02d28aa5753a6f76
songtao <songtao@snailgame.net>
2016-12-29 18:44:23 +0800

    update paganation component

--- page/page.js
+++ page/page.js
@@ -1,15 +1,44 @@
 Vue.component('pagination', {
-    template : '<div v-on:click="load">hello,{{url}}</div>',
-    props : ['url'],
-    methods : {
-        load : function() {
-            console.log(this.url);
+    template : '<ul class="pagination"><li v-for="n in page" :class="isCurrent(n)" @click="onPageClick"><a :page="n" href="javascript:void(0)">{{n}}</a></li></ul>',
+    props : ['url','size'],
+    data : function() {
+        return {
+            page : 10,
+            currentPage : 1,
+            pageSize : this.size || 12
         }
     },
-    created : function() {
-        console.log(this.$el)
+    methods : {
+        load: function() {
+            var self = this;
+            var url = 'http://127.0.0.1:5000/page';
+            Vue.http.jsonp(url, {
+                    params: {page : this.currentPage,pageSize : this.pageSize}
+                })
+                .then(this.onDataLoaded, this.onLoadFail);
+        },
+        isCurrent : function(n) {
+            return n == this.currentPage ? 'active' : '';
+        },
+        onPageClick : function(e) {
+            var page = parseInt(e.target.getAttribute('page'));
+            this.currentPage = page;
+            this.load()
+        },
+        onDataLoaded: function(data) {
+            if (data.body.code == 0) {
+                this.$emit('dataloaded',{data : data.body.data});
+            } else {
+                alert('频道列表读取失败！');
+                console.log(data.body.msg);
+            }
+        },
+        onLoadFail: function(e) {
+            alert(e);
+        },
+        formatData : function() {}
     },
-    mounted : function() {
-        console.log(this.$el)
+    created : function() {
+        this.load();
     }
 });
