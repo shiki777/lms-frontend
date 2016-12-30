3af019a75e53f152a1a6e8da0a5ff6ea7f654674
songtao <songtao@snailgame.net>
2016-12-30 11:56:44 +0800

    update channel update

---
+++ channel/update.js
@@ -0,0 +1,138 @@
+(function() {
+
+var id = 1;
+var roomid = window.location.search.match(/id=(.*)/);
+loadRoomInfo(roomid);
+
+function getId() {
+    return id++;
+}
+
+function loadRoomInfo(id) {
+    var url = 'http://127.0.0.1:5000/room';
+    Vue.http.jsonp(url,{params : {id : id}})
+    .then(function(data) {
+        if(data.body.code == 0){
+            var vmdata = formatRoomData(data.body.data);
+            createVm(vmdata);
+        } else {
+            alert('房间信息加载失败，请重试!');
+            console.log(data.body.msg);
+        }
+
+    })
+}
+
+function formatRoomData(data) {
+    var discount = [];
+    if(data.chargeStrategy.discount.length >0){
+        data.chargeStrategy.discount.map(function(item) {
+            discount.push({
+                discount : item.discount,
+                duration : item.mouth,
+                id : getId()
+            })
+        })
+    }
+    return {
+        name : data.name,
+        order : data.order,
+        price : data.chargeStrategy.price,
+        desc : data.desc,
+        tag : data.tag,
+        thumb : data.thumb,
+        dependencyCharge : data.dependencyCharge ? 1 : 0,
+        chargeStrategy  : discount
+    }
+}
+
+function createVm(data) {
+var vm = new Vue({
+    el : '#page',
+    data : data,
+    computed : {
+        changeShow : function() {
+            return parseInt(this.dependencyCharge,10) ? 'block' : 'none';
+        }
+    },
+    methods : {
+        addStrategy : function() {
+            this.chargeStrategy.push({
+                money : '',
+                duration : '',
+                unit : '',
+                id : getId()
+            })
+        },
+        /*监听收费策略删除，从数据中移除对应数据*/
+        onChargeRemove : function(e) {
+            this.removeStrategy(e.id);
+        },
+        submit : function(e) {
+            var url = 'http://127.0.0.1:5000/room/add';
+            var self = this;
+            Vue.http.post(url,{body : this.formatData()})
+            .then(function(data) {
+                if(data.body.code == 0){
+                    $('.ui.modal')
+                    .modal('show'); 
+                    // location.href= '';
+                } else {
+                    alert('提交失败：' + data.body.msg);
+                }
+
+            }, function(e) {
+                alert('提交失败');
+            })           
+            return false;
+        },
+        formatData : function() {
+            var res = {
+                name : this.name,
+                thumb : this.getThumb(),
+                desc : this.desc,
+                charge : parseInt(this.dependencyCharge,10) ? true : false,
+                order : this.order,
+                chargeStrategy : this.getChargeStrategy()
+            };
+            return res;
+        },
+        getChargeStrategy : function() {
+            if(parseInt(this.dependencyCharge,10) == 0){
+                return {
+                    price : 0,
+                    discount : []
+                };
+            }
+            var res = {
+                price : this.price,
+            }
+            discount = [];
+            this.$refs.charges.map(function(charge) {
+                if(charge.del == true) return;
+                discount.push({
+                    mouth : charge.d,
+                    discount : parseFloat(charge.m,10)
+                });
+            })     
+            res.discount = discount;       
+            return res;
+        },
+        getThumb : function() {
+            return this.$refs.thumbcom.imgurl;
+        },
+        removeStrategy : function(id) {
+            /*组件调用destroy后 refs没有同步减少，所以这么做，vue刚使用不熟悉*/
+            this.$refs.charges.map(function(charge) {
+                if(charge.id == id){
+                    charge.del = true;
+                }
+            })
+        }
+
+    }
+});   
+}
+
+
+})()
