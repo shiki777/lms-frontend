b2f7a0e45a4360f0b561c288aa28ddb1ced656bf
songtao <songtao@snailgame.net>
2017-01-10 17:57:32 +0800

    update 视频修改

---
+++ video/update.js
@@ -0,0 +1,87 @@
+(function() {
+
+    var id = 1;
+    var videoid = window.location.search.match(/id=(.*)/) ? window.location.search.match(/id=(.*)/)[1] : 0;
+    loadVideoInfo(videoid);
+
+    function getId() {
+        return id++;
+    }
+
+    function loadVideoInfo(id) {
+        var url = window.hosturl + '/lms/video/get';
+        Vue.http.jsonp(url, {
+                params: {
+                    id: id
+                }
+            })
+            .then(function(data) {
+                if (data.body.code == 0) {
+                    var vmdata = formatVideoData(data.body.data);
+                    createVm(vmdata);
+                } else {
+                    alert('房间信息加载失败，请重试!');
+                    console.log(data.body.msg);
+                }
+
+            })
+    }
+
+    function formatVideoData(data) {
+        return {
+            name: data.name,
+            url: data.downloadurl,
+            thumb: data.thumb,
+            desc: data.desc,
+            order: data.order
+        }
+    }
+
+    function createVm(data) {
+        var vm = new Vue({
+            el: '#page',
+            data: data,
+            methods: {
+                submit: function(e) {
+                    var url = window.hosturl + '/lms/video/update';
+                    var self = this;
+                    Vue.http.post(url, this.formatData(), {
+                            params: {
+                                id: videoid
+                            }
+                        })
+                        .then(function(data) {
+                            if (data.body.code == 0) {
+                                $('.ui.modal')
+                                    .modal('show');
+                                window.setTimeout(function() {
+                                    // window.location.reload();
+                                }, 1500);
+                            } else {
+                                alert('提交失败：' + data.body.msg);
+                            }
+
+                        }, function(e) {
+                            alert('提交失败');
+                        })
+                    return false;
+                },
+                formatData: function() {
+                    var res = {
+                        name: this.name,
+                        url: this.url,
+                        thumb: this.getThumb(),
+                        desc: this.desc,
+                        order: this.order
+                    };
+                    return res;
+                },
+                getThumb: function() {
+                    return this.$refs.thumbcom.imgurl;
+                }
+            }
+        });
+    }
+
+
+})()
