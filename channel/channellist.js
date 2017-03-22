4bdfb3d8fcf26ccf5d89228483b73cb58d872ed6
songtao <songtao@snailgame.net>
2017-03-22 11:49:02 +0800

    update 频道列表页

--- channel/channellist.js
+++ channel/channellist.js
@@ -3,24 +3,40 @@
 var vm = new Vue({
     el : '#page',
     data : {
-        channels : [],
-        url : window.hosturl + '/lms/channel/list'
+        channels : {},
+        tags : [],
+        url : window.hosturl + '/lms/channel/list',
+        selectTag : 'all'
+    },
+    computed : {
+      tagsFilter : function() {
+        if(this.selectTag == 'all'){
+          return this.tags;
+        }
+        return [this.selectTag];
+      }
     },
     methods : {
         pageLoaded : function(data) {
             this.channels = this.formatChannels(data.data);
         },
         formatChannels : function(data) {
-            var res = [];
+            var res = {};
+            var self = this;
             data.map(function(item) {
-                res.push({
+              var tag = item.tag || '默认标签(空标签)';
+              if(!res[tag]){
+                res[tag] = [];
+                self.tags.push(tag);
+              }
+                res[tag].push({
                   name : item.name,
                   thumb : item.thumb,
                   id : item.id,
                   link : window.hosturl + '/lms/page/channelupdate?id=' + item.id,
-                  tag : item.tag
+                  tag : tag
                 });
-            })
+            });
             return res;
         }
     }
